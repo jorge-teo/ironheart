227a42b2d2a89dc0582d76aaad7ee609df459881
root <root@JORGE.localdomain>
2019-05-27 18:19:13 +0200

    List View of Symptoms

--- routes/symptoms.routes.js
+++ routes/symptoms.routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Symptoms = require('../models/Symptoms.models')
 //const axios = require("axios")
 
 // Petición GET de los SYMPTOM
 
 router.get('/', (req, res, next)=> {
+  Symptoms.find()
+    .then(allSymptoms =>{allSymptoms.forEach(element => console.log(element.Name)) }) 
+    .catch(error => console.log(error))
   res.render('symptoms');
 });
 // router.get('/symptoms', (req, res, next) => {
