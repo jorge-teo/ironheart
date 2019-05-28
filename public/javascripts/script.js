037abb95957427d69a20e33e68a0b0c6ba6d6c16
Teo <teodorolg130@gmail.com>
2019-05-28 11:53:22 +0200

    actualizacion

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
+  document.getElementById("arm").onclick = () => alert("FUNCIONA!")
+
 
 }, false);
